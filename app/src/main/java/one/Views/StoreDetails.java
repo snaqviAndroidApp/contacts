81975ed646e46b9f304af2af8c1294026cc6a876
snaqviAndroidApp <naqvi.sdsu@gmail.com>
2018-05-14 03:27:42 -0500

    - Added new Activity that shows item selected by user in recyclerView - added dialing functionality when user taps on the 'phone' button provided left of the phone number displayed

--- app/src/main/java/one/Views/StoreDetails.java
+++ app/src/main/java/one/Views/StoreDetails.java
@@ -1,8 +1,15 @@
 package one.Views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +17,7 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import java.net.URISyntaxException;
+import java.text.MessageFormat;
 import java.util.List;
 
 import one.Model.BottlesD_Data;
@@ -17,31 +25,61 @@ import one.R;
 
 public class StoreDetails extends AppCompatActivity {
 
-    private List<BottlesD_Data> detailDataIn;                       //BackEnd Java DB Object list
-
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;        // Location n Contacts
     Intent iDetailRcv = null;
     String addrDIn = null;
+    TextView tvAdd, tvPh;
+    ImageView imgDLogoIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         iDetailRcv = getIntent();
+        tvAdd = findViewById(R.id.tVDetailAddr);
+        tvPh = findViewById(R.id.tVDetailPh);
+        imgDLogoIn = findViewById(R.id.imgDetailLog);
 
-//        iDetailRcv.getStringExtra("adb");
-        TextView tvAdd = findViewById(R.id.tVDetailAddr);
-        TextView tvPh = findViewById(R.id.tVDetailPh);
-        ImageView imgDLogoIn = findViewById(R.id.imgDetailLog);
+        CheckUserPermissions();                        // Check PERMISSIONS for SDK VER >= 23
 
-//
-//        String imgDUrl = iDetailRcv.getStringExtra("imgStrr");
         tvAdd.setText(iDetailRcv.getStringExtra("addrStrr"));
         tvPh.setText(iDetailRcv.getStringExtra("phStrr").split(":")[1]);
         Picasso.get().load(iDetailRcv.
                 getStringExtra("imgStrr"))
                 .into(imgDLogoIn
                 );
+    }
 
+    public void onPhone(View view) {
+        Intent intDial = new Intent(Intent.ACTION_DIAL);
+        intDial.setData(Uri.parse("tel:" + tvPh.getText().toString().trim()));
+        if (intDial.resolveActivity(getPackageManager()) != null) {
+            startActivity(intDial);
+        } else {
+            Log.e("phCall", "Can't resolve app for ACTION_DIAL Intent.");
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+    }
+
+    void CheckUserPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (
+                    (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                    ) {
+                requestPermissions(new String[]{
+                                Manifest.permission.CALL_PHONE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return;
+            }
+        }
     }
 }
